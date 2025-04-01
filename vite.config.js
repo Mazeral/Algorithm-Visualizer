f8b184cdcbb48b32c7c847e215fc0783762b8034
MohammadOSA <mohamedsiddig5026485@gmail.com>
2025-04-01 07:33:29 +0200

    reversed changes for vite configs.

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,12 @@ import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => { // Receive the 'mode' argument
-  const env = loadEnv(mode, process.cwd()); // Load environment variables
-
-  return {
-    plugins: [vue(), vueDevTools()],
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
+export default defineConfig({
+	base: '/Algorithm-Visualizer/',
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    base: env.NODE_ENV === 'production'
-      ? `/${env.Algorithm-Visualizer}/` // Replace with your actual repository name
-      : '/',
-  };
+  },
 });
